f60c9a3cc01ffa5c418b18a303319e1518e0beaa
Axel <112589455+AxelVandenHeuvel@users.noreply.github.com>
2024-10-21 21:27:40 -0600

    Adding Original Directory

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    darkMode: 'class', // Enable dark mode with the "class" strategy
+    content: [
+       './templates/**/*.html',
+    ],
+    theme: {
+      extend: {
+        // Custom theming and extensions can go here
+      },
+    },
+    plugins: [],
+  }
